ffbd2b9cb48180fb72603671afcf51d5c2cc7a0d
kirito96 <eagle1968@live.com>
2017-01-29 00:11:53 -0400

    Add Overclock MT6572 @1.4 Ghz

--- mediatek/platform/mt6572/kernel/core/include/mach/mt_cpufreq.h
+++ mediatek/platform/mt6572/kernel/core/include/mach/mt_cpufreq.h
@@ -19,19 +19,20 @@
 
 
 #define DVFS_D0              (1599000)   // KHz, OD
-#define DVFS_D1              (1404000)   // KHz, OD
+#define DVFS_D1              (1400000)   // KHz, OD
 #define DVFS_D2              (1300000)   // KHz, OD
-#define DVFS_D3              (1209000)   // KHz, OD
-#define DVFS_F1              (1001000)   // KHz
-#define DVFS_F2              (806000)    // KHz
-#define DVFS_F3              (598000)    // KHz
+#define DVFS_D3              (1200000)   // KHz, OD
+#define DVFS_F1              (1000000)   // KHz
+#define DVFS_F2              (800000)    // KHz
+#define DVFS_F3              (600000)    // KHz
+#define DVFS_F4              (500000)    // Khz
 
 #define DVFS_V0              (1250)  // mV, OD
 #define DVFS_V1              (1150)  // mV
 
 #define DVFS_MIN_VCORE       (1150)
 #define DEFAULT_FREQ         (1001000)   // KHz
-#define NOR_MAX_FREQ         (1001000)   // KHz
+#define NOR_MAX_FREQ         (1404000)   // KHz
 
 #define CORE_NUM             (2)
 #define VCORE_NUM            (2)
