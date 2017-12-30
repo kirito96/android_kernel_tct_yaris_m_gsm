93b9fad7b605ec7c7c69b23a52e5b6a557d2fb0b
kirito96 <eagle1968@live.com>
2017-12-30 16:28:59 -0400

    Update partitions to stock

--- arch/arm/mach-mt6572/yaris_m_gsm/common/nand_device_list.h
+++ arch/arm/mach-mt6572/yaris_m_gsm/common/nand_device_list.h
@@ -1,32 +1,65 @@
+#ifndef CUST_NAND_H 
+#define CUST_NAND_H
 
-#ifndef __NAND_DEVICE_LIST_H__
-#define __NAND_DEVICE_LIST_H__
+#include "typedefs.h"
 
-#define NAND_MAX_ID		5
-#define CHIP_CNT		4
-#define RAMDOM_READ		(1<<0)
-#define CACHE_READ		(1<<1)
+#if 0
+#define RAMDOM_READ 1<<0
+#define CACHE_READ  1<<1
 
 typedef struct
 {
-   u8 id[NAND_MAX_ID];
-   u8 id_length;
-   u8 addr_cycle;
-   u8 iowidth;
-   u16 totalsize;
+   u16 id;          //deviceid+menuid
+   u8  addr_cycle;
+   u8  iowidth;
+   u16 totalsize;   
    u16 blocksize;
    u16 pagesize;
-   u16 sparesize;
    u32 timmingsetting;
-   u8 devciename[30];
-   u32 advancedmode;
+   char devciename[14];
+   u32 advancedmode;   //
 }flashdev_info,*pflashdev_info;
 
-static const flashdev_info gen_FlashTable[]={
-	{{0x2C,0xBC,0x90,0x66,0x54}, 5,5, 16,512,256,4096,224,0x10805113,"MT29C4G48MAAHBAAKS_5WT",0}, 
-	{{0xEC,0xBC,0x00,0x6A,0x56}, 5,5, 16,512,256,4096,128,0x10C08123,"K524G2GACH_B050 ",1}, 
-	{{0xAD,0xBC,0x80,0x16,0x20}, 5,5, 16,512,256,4096,128,0x21014133,"H9DA4VH2GJAMCR_4EM",0}, 
-	{{0x98,0xBC,0x90,0x66,0x00}, 4,5, 16,512,256,4096,128,0x10814114,"TYBC0A111557KC",0}, 
+static const flashdev_info g_FlashTable[]={
+	//micro
+	{0xAA2C,  5,  8,  256,  128,  2048,  0x01113,  "MT29F2G08ABD",  0},
+	{0xB12C,  4,  16, 128,  128,  2048,  0x01113,  "MT29F1G16ABC",  0},
+	{0xBA2C,  5,  16, 256,  128,  2048,  0x01113,  "MT29F2G16ABD",  0},	
+	{0xAC2C,  5,  8,  512,	128,  2048,  0x01113,  "MT29F4G08ABC",	0},
+    {0xA12C,  4,  8,  128,  128,  2048,  0x01113,  "MT29F1G08ABB",	0},
+    {0xBC2C,  5,  16, 512, 128,  2048,  0x01112,  "MT29F4G16ABBDA",	0},
+	//samsung 
+	{0xBAEC,  5,  16, 256,  128,  2048,  0x01123,  "K522H1GACE",    0},
+	{0xBCEC,  5,  16, 512,  128,  2048,  0x01123,  "K524G2GACB",    RAMDOM_READ},
+	{0xDAEC,  5,  8,  256,  128,  2048,  0x33222,  "K9F2G08U0A",    RAMDOM_READ},
+	{0xF1EC,  4,  8,  128,  128,  2048,  0x01123,  "K9F1G08U0A",    RAMDOM_READ},
+	{0xAAEC,  5,  8,  256,  128,  2048,  0x01123,  "K9F2G08R0A",    0},
+    //hynix
+	{0xD3AD,  5,  8,  1024, 256,  2048,  0x44333,  "HY27UT088G2A",  0},
+	{0xA1AD,  4,  8,  128,  128,  2048,  0x01123,  "H8BCSOPJOMCP",  0},
+	{0xBCAD,  5,  16, 512,  128,  2048,  0x01123,  "H8BCSOUNOMCR",  0},
+	{0xBAAD,  5,  16, 256,  128,  2048,  0x01123,  "H8BCSOSNOMCR",  0},
+	//toshiba
+	{0x9598,  5,  16, 816,  128,  2048,  0x00113,  "TY9C000000CMG", 0},
+	{0x9498,  5,  16, 375,  128,  2048,  0x00113,  "TY9C000000CMG", 0},
+    {0xBC98,  5,  16, 512, 128,  2048,  0x02113,  "TY58NYG2S8E",	0},
+	{0xC198,  4,  16, 128,  128,  2048,  0x44333,  "TC58NWGOS8C",   0},
+	{0xBA98,  5,  16, 256,  128,  2048,  0x02113,  "TC58NYG1S8C",   0},
+	//st-micro
+	{0xBA20,  5,  16, 256,  128,  2048,  0x01123,  "ND02CGR4B2DI6", 0},
+
+	// elpida
+	{0xBC20,  5,  16, 512,  128,  2048,  0x01123,  "04GR4B2DDI6",   0},
+	{0x0000,  0,  0,  0,	0,	  0,	 0, 	   "xxxxxxxxxxxxx", 0}
 };
+#endif
+
+#define NFI_DEFAULT_ACCESS_TIMING        (0x44333)
+
+//preloader only support 1 cs
+//#define NFI_CS_NUM                  	(2)
+#define NFI_DEFAULT_CS				(0)
+
+#define USE_AHB_MODE                	(1)
 
 #endif
